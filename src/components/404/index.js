4ff6096dd45880adde468350124467fcf47e3648
bdbose <bdbose123@gmail.com>
2020-09-15 16:16:27 +0530

    fix: css

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -5,7 +5,7 @@ import './style.scss';
 
 const NoPage = () => {
 	return (
-		<div className='err'>
+		<div className='error-msg'>
 			<h1 className='glitch' data-text='404. Page Not Found !'>
 				404. Page Not Found !
 			</h1>
